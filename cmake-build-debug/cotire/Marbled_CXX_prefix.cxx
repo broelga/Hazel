a85b385b620511bc554ce606dc8fe2f028130da1
broel <joelthomas.e@gmail.com>
2021-06-24 20:54:20 -0700

    File management

--- cmake-build-debug/cotire/Marbled_CXX_prefix.cxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.8.1 generated file */
-/* /Users/ibroel/dev/Marble/cmake-build-debug/cotire/Marbled_CXX_prefix.cxx */
-#ifdef __cplusplus
-#include "/Users/ibroel/dev/Marble/Marble/src/mrblpch.h"
-#endif
--- cmake-build-debug/cotire/Marbled_CXX_prefix.hxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.8.1 generated file */
-/* /Users/ibroel/dev/Marble/cmake-build-debug/cotire/Marbled_CXX_prefix.hxx */
-#ifdef __cplusplus
-#include "/Users/ibroel/dev/Marble/cmake-build-debug/cotire/Marbled_CXX_prefix.cxx"
-#endif
