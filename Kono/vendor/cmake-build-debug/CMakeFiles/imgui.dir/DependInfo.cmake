03c2867d5adb7c406c5d15581c2beda904da11a1
broel <joelthomas.e@gmail.com>
2021-07-15 09:47:22 -0700

    Created new branch to optimized project file-structure. This branch will be merged with main and then deleted.

---
+++ Kono/vendor/cmake-build-debug/CMakeFiles/imgui.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ibroel/dev/Kono/Kono/vendor/imgui/backends/imgui_impl_glfw.cpp" "/Users/ibroel/dev/Kono/Kono/vendor/cmake-build-debug/CMakeFiles/imgui.dir/imgui/backends/imgui_impl_glfw.o"
+  "/Users/ibroel/dev/Kono/Kono/vendor/imgui/imgui.cpp" "/Users/ibroel/dev/Kono/Kono/vendor/cmake-build-debug/CMakeFiles/imgui.dir/imgui/imgui.o"
+  "/Users/ibroel/dev/Kono/Kono/vendor/imgui/imgui_demo.cpp" "/Users/ibroel/dev/Kono/Kono/vendor/cmake-build-debug/CMakeFiles/imgui.dir/imgui/imgui_demo.o"
+  "/Users/ibroel/dev/Kono/Kono/vendor/imgui/imgui_draw.cpp" "/Users/ibroel/dev/Kono/Kono/vendor/cmake-build-debug/CMakeFiles/imgui.dir/imgui/imgui_draw.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../imgui"
+  "../imgui/examples/example_glfw_opengl3"
+  "../GLFW/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/Kono/vendor/Glad/glad-config.cmake
@@ -0,0 +1,1 @@
+include(/Users/ibroel/dev/Kono/Kono/vendor/Glad/cmake-build-debug/glad-targets.cmake)
