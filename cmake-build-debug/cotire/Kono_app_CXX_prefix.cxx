c2fea32aca46100afdc04463bba17f4822755eda
broel <joelthomas.e@gmail.com>
2021-07-15 10:13:48 -0700

    .gitignore now working

---
+++ cmake-build-debug/cotire/Kono_app_CXX_prefix.cxx
@@ -0,0 +1,5 @@
+/* cotire.cmake 1.8.1 generated file */
+/* /Users/ibroel/dev/Kono/cmake-build-debug/cotire/Kono_app_CXX_prefix.cxx */
+#ifdef __cplusplus
+#include "/Users/ibroel/dev/Kono/Kono/src/knpch.h"
+#endif
